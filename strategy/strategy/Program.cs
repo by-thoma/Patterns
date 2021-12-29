5c7b104fe63bb2c31556a7bdbd9fea59fd68db60
Tamara Tsogoeva <63509284+by-thoma@users.noreply.github.com>
2021-12-29 17:58:23 +0300

    Add files via upload

---
+++ strategy/strategy/Program.cs
@@ -0,0 +1,65 @@
+﻿class Map
+{
+    public void road() { }
+}
+
+class AutoRoad: Map
+{
+    new public void road() 
+    {
+        Console.WriteLine("Маршрут для авто построен"); }
+}
+
+class WalkingRoad: Map
+{
+    new public void road()
+    {
+        Console.WriteLine("Маршрут для пешехода построен");
+    }
+}
+
+class BusRoad : Map
+{
+    new public void road()
+    {
+        Console.WriteLine("Маршрут для общественного транспорта построен");
+    }
+}
+
+class AttractionsRoad : Map
+{
+    new public void road()
+    {
+        Console.WriteLine("Маршрут для осмотра достопримечательностей построен");
+    }
+}
+
+class Context
+{
+    string type_road;
+    public Context(string type_road)
+    {
+        this.type_road = type_road;
+    }
+
+    public void search_road()
+    {
+        if (type_road == "авто") { AutoRoad way = new AutoRoad(); way.road(); }
+        else if (type_road == "пешеход") { WalkingRoad way = new WalkingRoad(); way.road(); }
+        else if (type_road == "автобус") { BusRoad way = new BusRoad(); way.road(); }
+        else if (type_road == "достопримечательность") { AttractionsRoad way = new AttractionsRoad(); way.road(); }
+        else Console.WriteLine("Невозможно найти маршрут");
+    }
+}
+
+class Program
+{
+    static void Main(string[] str)
+    {
+        Context context_1 = new Context("пешеход");
+        Context context_2 = new Context("автобус");
+
+        context_1.search_road();
+        context_2.search_road();
+    }
+}
